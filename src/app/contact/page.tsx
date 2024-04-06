8896116609f1a8423cc2dbc04355601ec9c1c13e
Kyle Garcia <kylewebdev@gmail.com>
2024-04-06 03:05:49 -0700

    Updated metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,18 +3,20 @@ import React from "react";
 const ContactPage = () => {
 	return (
 		<div className="w-full h-full min-h-screen py-24 px-8 flex items-center justify-center">
-			<h1 className="text-6xl font-normal tracking-tighter lg:text-8xl">
-				<a
-					className="block break-all"
-					href="mailto:kylewebdev@gmail.com"
-					target="_blank"
-				>
-					kylewebdev@gmail.com
-				</a>
-				{/* <sup className="relative -top-8 left-2 text-base tracking-normal lg:-top-16 lg:left-3 lg:text-2xl">
-					[3]
-				</sup> */}
-			</h1>
+			<div className="flex flex-col gap-6">
+				<h1 className="text-6xl font-normal tracking-tighter lg:text-8xl">
+					<a
+						className="block break-all"
+						href="mailto:kylewebdev@gmail.com"
+						target="_blank"
+					>
+						kylewebdev@gmail.com
+					</a>
+				</h1>
+				{/* <p className="max-w-[66ch] leading-7 text-pretty text-slate-400">
+					Got questions? Need a web wizard? Your message awaits!
+				</p> */}
+			</div>
 		</div>
 	);
 };
