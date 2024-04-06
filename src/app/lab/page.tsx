67f65e49f6f932be922cf67e6f4c9f81d8417d2b
Kyle Garcia <kylewebdev@gmail.com>
2024-04-06 02:00:36 -0700

    Added GSAP, and updated basic style of website

--- src/app/lab/page.tsx
+++ src/app/lab/page.tsx
@@ -1,12 +1,16 @@
-export default function Home() {
+import React from "react";
+
+const LabPage = () => {
 	return (
 		<div className="w-full h-full min-h-screen py-24 px-8">
 			<h1 className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-7xl font-normal tracking-tighter lg:text-9xl">
-				Index
+				Lab
 				<sup className="relative -top-8 left-2 text-base tracking-normal lg:-top-16 lg:left-3 lg:text-2xl">
-					[0]
+					[4]
 				</sup>
 			</h1>
 		</div>
 	);
-}
+};
+
+export default LabPage;
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
+import styles from "./Menu.module.css";
+
+const menuItems = [
+	{ path: "/", label: "Index" },
+	{ path: "/work", label: "Work" },
+	{ path: "/about", label: "About" },
+	{ path: "/contact", label: "Contact" },
+];
+
+const Menu = () => {
+	const containerRef = useRef<HTMLDivElement>(null);
+	const [isMenuOpen, setIsMenuOpen] = useState<Boolean>(false);
+	const timeline = useRef<gsap.core.Timeline>();
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
+
+	useGSAP(
+		() => {
+			gsap.set(".menu-item--holder", { y: "100%" });
+
+			timeline.current = gsap
+				.timeline({ paused: true })
+				.to(".overlay", {
+					duration: 1.5,
+					clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0 100%)",
+					ease: "power4.inOut",
+				})
+				.to(".menu-item--holder", {
+					y: 0,
+					duration: 1,
+					stagger: 0.1,
+					ease: "power4.out",
+					delay: -0.75,
+				});
+		},
+		{ scope: containerRef }
+	);
+
+	useEffect(() => {
+		if (!timeline.current) return;
+
+		if (isMenuOpen) {
+			timeline.current.play();
+		} else {
+			timeline.current.reverse();
+		}
+	}, [isMenuOpen]);
+
+	return (
+		<div className="menu-container" ref={containerRef}>
+			<div className="fixed top-0 left-0 w-screen p-8 flex justify-between items-center z-10">
+				<div className="uppercase font-mono">
+					<Link href="/">KyleWebDev</Link>
+				</div>
+				<div
+					className="cursor-pointer uppercase font-mono"
+					onClick={toggleMenu}
+				>
+					<p>Menu</p>
+				</div>
+			</div>
+
+			{/* Overlay */}
+			<div className="overlay fixed top-0 left-0 w-screen h-screen p-8 bg-slate-200 text-black grid grid-cols-[1fr_4fr_1fr] grid-rows-[88px_1fr_1fr] gap-4 z-10">
+				<div className="flex flex-col justify-between h-full row-start-1 row-end-4 col-start-1 col-end-1">
+					<div className="uppercase font-mono">
+						<Link href="/">KyleWebDev</Link>
+					</div>
+					<div>
+						<a
+							className="block"
+							href="https://twitter.com/kylewebdev_"
+							target="_blank"
+						>
+							X &#8599;
+						</a>
+						<a
+							className="block"
+							href="https://github.com/kylewebdev"
+							target="_blank"
+						>
+							GitHub &#8599;
+						</a>
+						<a
+							className="block"
+							href="https://www.linkedin.com/in/kylewebdev"
+							target="_blank"
+						>
+							LinkedIn &#8599;
+						</a>
+						<a
+							className="block"
+							href="mailto:kylewebdev@gmail.com"
+							target="_blank"
+						>
+							Email &#8599;
+						</a>
+					</div>
+				</div>
+
+				<div className="flex flex-col justify-between h-full col-start-1 row-start-2 lg:col-start-2">
+					<div className="flex flex-col gap-1">
+						{menuItems.map((item, index) => (
+							<div key={index} className="overflow-hidden">
+								<div className="menu-item--holder">
+									<Link
+										className="text-6xl lg:text-9xl font-extralight uppercase -tracking-wider"
+										href={item.path}
+										onClick={toggleMenu}
+									>
+										{item.label}
+									</Link>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+
+				<div className="flex flex-col justify-between items-end h-full row-start-1 row-end-4 col-start-2 col-end-2 lg:col-start-3 lg:col-end-3">
+					<div className="uppercase font-mono" onClick={toggleMenu}>
+						<p>Close</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Menu;
