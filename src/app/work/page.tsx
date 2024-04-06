09ab7d3e3e5f76cc8c042c720ea2190ea8052d89
Kyle Garcia <kylewebdev@gmail.com>
2024-04-06 03:34:50 -0700

    Updated Responsive

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const WorkPage = () => {
 	return (
-		<div className="w-full h-full min-h-screen py-24 px-8 flex items-center justify-center">
+		<div className="w-full h-full min-h-dvh py-24 px-8 flex items-center justify-center">
 			<div className="flex flex-col gap-6">
 				<h1 className="font-normal tracking-tighter text-6xl lg:text-8xl text-balance">
 					Work
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -77,69 +77,71 @@ const Menu = () => {
 			</div>
 
 			{/* Overlay */}
-			<div className="overlay fixed top-0 left-0 w-screen h-dvh p-8 bg-slate-200 text-black grid grid-cols-[1fr_1fr] lg:grid-cols-[1fr_4fr_1fr] grid-rows-[88px_1fr_1fr] gap-4 z-10">
-				<div className="flex flex-col justify-between h-full row-start-1 row-end-4 col-start-1 col-end-1">
-					<div className="uppercase font-mono">
-						<Link href="/" onClick={closeMenu}>
-							KyleWebDev
-						</Link>
+			<div className="overlay overflow-y-auto fixed top-0 left-0 w-screen h-dvh p-8 bg-slate-200 text-black z-10">
+				<div className="grid grid-cols-[1fr_4fr_1fr] grid-rows-[88px_2fr_1fr] gap-4 sm:h-full">
+					<div className="flex flex-col justify-between h-full row-start-1 row-end-5 col-start-1 col-end-1">
+						<div className="uppercase font-mono">
+							<Link href="/" onClick={closeMenu}>
+								KyleWebDev
+							</Link>
+						</div>
+						<div>
+							<a
+								className="block"
+								href="https://twitter.com/kylewebdev_"
+								target="_blank"
+							>
+								X &#8599;
+							</a>
+							<a
+								className="block"
+								href="https://github.com/kylewebdev"
+								target="_blank"
+							>
+								GitHub &#8599;
+							</a>
+							<a
+								className="block"
+								href="https://www.linkedin.com/in/kylewebdev"
+								target="_blank"
+							>
+								LinkedIn &#8599;
+							</a>
+							<a
+								className="block"
+								href="mailto:kylewebdev@gmail.com"
+								target="_blank"
+							>
+								Email &#8599;
+							</a>
+						</div>
 					</div>
-					<div>
-						<a
-							className="block"
-							href="https://twitter.com/kylewebdev_"
-							target="_blank"
-						>
-							X &#8599;
-						</a>
-						<a
-							className="block"
-							href="https://github.com/kylewebdev"
-							target="_blank"
-						>
-							GitHub &#8599;
-						</a>
-						<a
-							className="block"
-							href="https://www.linkedin.com/in/kylewebdev"
-							target="_blank"
-						>
-							LinkedIn &#8599;
-						</a>
-						<a
-							className="block"
-							href="mailto:kylewebdev@gmail.com"
-							target="_blank"
-						>
-							Email &#8599;
-						</a>
-					</div>
-				</div>
 
-				<div className="flex flex-col justify-between h-full col-start-1 row-start-2 lg:col-start-2">
-					<div className="flex flex-col gap-1">
-						{menuItems.map((item, index) => (
-							<div key={index} className="overflow-hidden">
-								<div className="menu-item--holder">
-									<Link
-										className="text-5xl sm:text-6xl lg:text-9xl font-extralight uppercase -tracking-wider"
-										href={item.path}
-										onClick={toggleMenu}
-									>
-										{item.label}
-									</Link>
+					<div className="flex flex-col justify-between h-full col-start-1 row-start-2 lg:col-start-2">
+						<div className="flex flex-col gap-1">
+							{menuItems.map((item, index) => (
+								<div key={index} className="overflow-hidden">
+									<div className="menu-item--holder">
+										<Link
+											className="text-5xl sm:text-6xl lg:text-9xl font-extralight uppercase -tracking-wider"
+											href={item.path}
+											onClick={toggleMenu}
+										>
+											{item.label}
+										</Link>
+									</div>
 								</div>
-							</div>
-						))}
+							))}
+						</div>
 					</div>
-				</div>
 
-				<div className="flex flex-col justify-between items-end h-full row-start-1 row-end-4 col-start-2 col-end-2 lg:col-start-3 lg:col-end-3">
-					<div
-						className="cursor-pointer uppercase font-mono"
-						onClick={toggleMenu}
-					>
-						<p>Close</p>
+					<div className="flex flex-col justify-between items-end h-full row-start-1 row-end-5 col-start-3 col-end-3">
+						<div
+							className="cursor-pointer uppercase font-mono"
+							onClick={toggleMenu}
+						>
+							<p>Close</p>
+						</div>
 					</div>
 				</div>
 			</div>
