7bd1cd0fbbd8d351ab49ef2e8fbfac83359747ed
Kyle Garcia <kylewebdev@gmail.com>
2024-04-06 03:09:30 -0700

    Fixed Grammar

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,8 +15,8 @@ const AboutPage = () => {
 
 				<p className="max-w-[66ch] leading-7 text-pretty text-slate-400">
 					I&apos;ve always had a knack for figuring out how things
-					work on the web, and it turns out, I turn my passion for web
-					design into helping businesses build their dream websites.
+					work on the web, and it turns out, I love turning that
+					passion into helping businesses build their dream websites.
 				</p>
 			</div>
 		</div>
