e545a524ecf37ad98df9ef381488239cc1b24756
Kyle Garcia <kylewebdev@gmail.com>
2024-04-06 03:23:01 -0700

    Fixed css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<div className="w-full h-full min-h-screen py-24 px-8 flex items-center justify-center">
+		<div className="w-full h-full min-h-dvh py-24 px-8 flex items-center justify-center">
 			<h1 className="max-w-[14ch] font-normal tracking-tighter text-6xl lg:text-9xl text-balance">
 				I Craft Modern, User-Focused Web Experiences
 			</h1>
